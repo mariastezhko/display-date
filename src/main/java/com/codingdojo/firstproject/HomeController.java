fcc53ad01db24fb597c3c08da9cc4753ccc0d48e
Maria Stezhko <stezhko@gmail.com>
2018-06-11 16:44:28 -0700

    Initial commit

---
+++ src/main/java/com/codingdojo/firstproject/HomeController.java
@@ -0,0 +1,23 @@
+package com.codingdojo.firstproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/date")
+	public String date() {
+		return "date.jsp";
+	}
+	
+	@RequestMapping("/time")
+	public String time() {
+		return "time.jsp";
+	}
+
+}
